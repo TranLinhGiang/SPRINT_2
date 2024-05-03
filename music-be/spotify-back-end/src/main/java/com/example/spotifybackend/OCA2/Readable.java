ea100468fa4efc5629d67764c5167e3e764e5a8a
Trần Linh Giang <gt07839@gmail.com>
2024-05-03 14:53:29 +0700

    update yêu thích front end date 3-5-2024

--- music-be/spotify-back-end/src/main/java/com/example/spotifybackend/OCA2/Readable.java
+++ music-be/spotify-back-end/src/main/java/com/example/spotifybackend/OCA2/Readable.java
@@ -1,7 +1,7 @@
-package com.example.spotifybackend.OCA2;
-
-public interface Readable {
-    public void readBook();
-    public void setBookMark();
-}
-
+//package com.example.spotifybackend.OCA2;
+//
+//public interface Readable {
+//    public void readBook();
+//    public void setBookMark();
+//}
+//
