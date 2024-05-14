71940635d226f2bcd42650764876f78226f1afaf
Trần Linh Giang <gt07839@gmail.com>
2024-05-14 14:53:04 +0700

    update 14-05-2024

--- music-be/spotify-back-end/src/main/java/com/example/spotifybackend/demo/Demo.java
+++ music-be/spotify-back-end/src/main/java/com/example/spotifybackend/demo/Demo.java
@@ -1,2 +1,18 @@
-package com.example.spotifybackend.demo;public class Demo {
+package com.example.spotifybackend.demo;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        String name;
+        Double point;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name student: ");
+        name = scanner.nextLine();
+        System.out.println("Enter point student: ");
+        point = scanner.nextDouble();
+
+        System.out.println("< Tên sinh viên > : " + name + " , < Điểm sinh viên >: " + point);
+
+    }
 }
